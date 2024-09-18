8e0236d364731ffc707538c49766155b52ec296f
umamalam <155060609+umamalam@users.noreply.github.com>
2024-09-18 18:59:31 +0530

    Add files via upload

---
+++ zero.py
@@ -0,0 +1,10 @@
+while True:
+    n=int(input('enter the number'))
+    if n<0:
+        print("Number is Negative",n)
+    elif n>0:
+        print("Number is Positive",n)
+    elif n==0:
+        break
+    
+print("Program Terminate")
