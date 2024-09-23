b434687e35fce5ef3b42cc0b5485ddac06356f83
umamalam <155060609+umamalam@users.noreply.github.com>
2024-09-23 15:32:57 +0530

    Add files via upload

---
+++ sa.py
@@ -0,0 +1,8 @@
+slry=int(input('enter the salary='))
+pf= slry*12/100
+rs=slry-pf
+hr=10*rs/100
+ed=20*rs/100
+print("amount deducted as provident fund=",pf)
+print("amount spent on rent and education=",hr) 
+print("amount spent in education=",ed)
